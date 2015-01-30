672474354121729203ecfd71f28d9cdb779e39a3
Connor Bailey <cbbcbail@gmail.com>
2015-01-30 14:27:57 -0700

    Move lib back to eco in compi

--- compi.sh
+++ compi.sh
@@ -1,12 +1,11 @@
-echo compiREBORN
 echo Returning from the ashes to live another day…
 cd ~/Documents/eco
 echo Compiling...
-javac -cp .:../lib/* src/**/*.java
+javac -cp .:/lib/* src/**/*.java
 clear
 echo Compiled...
 cd ~/Documents/eco/src
 sleep 1
 echo Running…
 clear
-java -cp .:../../lib/* -Djava.library.path=../native eco/Main
+java -cp .:../lib/* -Djava.library.path=../native eco/Main
