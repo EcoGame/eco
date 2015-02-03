b3691f2c000a5b2bfda7ff5259e33fa792923d5d
Connor <cbbcbail@gmail.com>
2015-02-03 14:21:25 -0700

    Update Wheat.java

--- src/eco/Wheat.java
+++ src/eco/Wheat.java
@@ -4,17 +4,37 @@ public class Wheat{
 
     public static int tWheat;
     public static int wheatPrice;
+    public static int unemployedFarmers = 0;
+    public static int employedFarmers = 0;
     
-    public static int tWheat(int fPop, int tAcres, int unemployedFarmers, int employedFarmers) {
+    public static int farmPacks(int tAcres) {
         
         int farmPacks = tAcres/5;
+        return farmPacks;
+        
+    }
+    
+    public static int unemployedFarmers(int farmPacks, int fPop) {
+        
         unemployedFarmers = fPop - farmPacks;
         
         if(unemployedFarmers < 0) {
             unemployedFarmers = 0;
         }
         
+        return unemployedFarmers;
+        
+    }
+    
+    public static int employedFarmers(int fPop, int unemployedFarmers) {
+        
         employedFarmers = (fPop - unemployedFarmers);
+        return employedFarmers;
+        
+    }
+    
+    public static int tWheat(int fPop, int tAcres, int employedFarmers) {
+        
         tWheat = employedFarmers * 40;
         return tWheat;
         
