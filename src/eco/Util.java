02de47a42b0d8ede4203f1bc4e9d2dfbf9211cc7
Connor <cbbcbail@gmail.com>
2015-02-10 14:28:49 -0700

    Update Util.java

--- src/eco/Util.java
+++ src/eco/Util.java
@@ -23,57 +23,58 @@ public class Util {
 	*/
 
 	 public static void readSave(){
-	 String path = "../save.txt";
-	 Scanner s = null;
-	 try {
-	 	s = new Scanner(new File(path));
-	 } catch (FileNotFoundException e){
-	 	readError();
-	 	return;
-	 }
-	 	 ArrayList<String> list = new ArrayList<String>();
-	 try{
-	 while (s.hasNext()){
-	 	list.add(s.next());
-	 }
-	 s.close();
-	 }
-	 catch (Exception e){
-	 	readError();
-	 return;
-	 }
-	 try{
-	 	for (String str : list){
-	 		str = str.replace(System.getProperty("line.separator"), "");
-	 	}
-	 	Warrior.wPop = Integer.valueOf(list.get(0));
-	 	//System.out.println(Warrior.wPop);
-	 	Farmer.fPop = Integer.valueOf(list.get(1));
-	 	//System.out.println(Farmer.fPop);
-	 	Main.tAcres = Integer.valueOf(list.get(2));
-	 	//System.out.println(Main.tAcres);
-	 	Wheat.employedFarmers = Integer.valueOf(list.get(3));
-	 	//System.out.println(Wheat.employedFarmers);
-	 	Wheat.unemployedFarmers = Integer.valueOf(list.get(4));
-	 	//System.out.println(Wheat.unemployedFarmers);
-	 	Money.tMoney = Integer.valueOf(list.get(5));
-	 	//System.out.println(Money.tMoney);
-	 	Main.wheatPrice = Integer.valueOf(list.get(6));
-	 	//System.out.println(Main.wheatPrice);
-	 	Wheat.tWheat = Integer.valueOf(list.get(7));
-	 	readSuccess();
-	 	
-	 } catch(Exception e){
-	 	readError();
-	 }
-	 return;
-	}
+         String path = "../save.txt";
+         Scanner s = null;
+         try {
+             s = new Scanner(new File(path));
+         } catch (FileNotFoundException e){
+             readError();
+             return;
+         }
+            ArrayList<String> list = new ArrayList<String>();
+         try{
+             while (s.hasNext()){
+                 list.add(s.next());
+             }
+             s.close();
+         }
+         catch (Exception e){
+             readError();
+             return;
+         }
+         try{
+            for (String str : list){
+            str = str.replace(System.getProperty("line.separator"), "");
+            }
+             Warrior.wPop = Integer.valueOf(list.get(0));
+             //System.out.println(Warrior.wPop);
+             Farmer.fPop = Integer.valueOf(list.get(1));
+             //System.out.println(Farmer.fPop);
+             Main.tAcres = Integer.valueOf(list.get(2));
+             //System.out.println(Main.tAcres);
+             Wheat.employedFarmers = Integer.valueOf(list.get(3));
+             //System.out.println(Wheat.employedFarmers);
+             Wheat.unemployedFarmers = Integer.valueOf(list.get(4));
+             //System.out.println(Wheat.unemployedFarmers);
+             Money.tMoney = Integer.valueOf(list.get(5));
+             //System.out.println(Money.tMoney);
+             Main.wheatPrice = Integer.valueOf(list.get(6));
+             //System.out.println(Main.wheatPrice);
+             Wheat.tWheat = Integer.valueOf(list.get(7));
+             readSuccess();
+         } catch(Exception e){
+             readError();
+         }
+         return;
+     }
+    
 	public static void readError(){
 		World.messages.add(new Message("------------------------------------------", 100, 100, 300));
 	 	World.messages.add(new Message("Failed to load save!", 100, 130, 300));
 	 	World.messages.add(new Message("The file either disappeared or is corrupt!", 100, 160, 300));
 	 	World.messages.add(new Message("------------------------------------------", 100, 190, 300));
 	}
+    
 	public static void readSuccess(){
 		World.messages.add(new Message("----------------------------------", 100, 100, 300));
 	 	World.messages.add(new Message("Loaded game state from save file!", 100, 130, 300));
