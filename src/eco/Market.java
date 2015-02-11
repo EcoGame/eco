edd318248440486390285dbf477e8f3ce1ec0b50
Nate <4u2njoy.nathan@gmail.com>
2015-02-11 14:18:37 -0700

    Update Market.java

--- src/eco/Market.java
+++ src/eco/Market.java
@@ -2,6 +2,8 @@ package eco;
 
 public class Market {
 	static int[] prices = new int[75];
+	static int[] pop = new int[75];
+	static int[] year = new int[75];
     public static int wheatPrice(int pastPrice){
         int newPrice = 1;
         int chance = (Main.randInt(0,100));
@@ -19,14 +21,43 @@ public class Market {
         int y = 620;
         int x = 854;
         int shift = 100;
+        //Render.awtFont = Render.awtFont.deriveFont(24f); // set font size
+		//Render.font = new Render.TrueTypeFont(Render.awtFont, false);
+        
         prices[74] = newPrice;
+        //if(Main.year % 35 == 0){
+        //	year[74] = Main.year;
+        //}
+        
         for(int i=0; i<prices.length; i++){
-            	World.messages.add(new Message(".", (x - shift) + i, (y - shift) - prices[i], 1));
+            	World.messages.add(new Message("\u25A0 .", (x - shift) + i, (y - shift) - (prices[i]), 1));
+            	//if(year[i] != 0)
+            	//	World.messages.add(new Message("" + year[i], (x - shift) + i, (y - shift), 1));
          }
+         
 
     	for(int i=0; i<prices.length - 1; i++){
             prices[i] = prices[i + 1];
         }
+        
+        //for(int i=0; i<year.length - 1; i++){
+          //  year[i] = year[i + 1];
+        //}
+        
+        
+        //World.messages.add(new Message("" + year, (x - shift), (y - shift - 40), 1));
+        
+
+        pop[74] = Warrior.wPop + Farmer.fPop;
+        for(int i=0; i<pop.length; i++){
+            	World.messages.add(new Message("\u25A0 .", (x - 80 - shift) + i, (y - shift) - (pop[i]), 1));
+         }
+
+
+    	for(int i=0; i<pop.length - 1; i++){
+            pop[i] = pop[i + 1];
+        }
+
 
 		return newPrice;
         
