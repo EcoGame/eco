54c6f6836d04109001deb3ad00139536018b767b
Connor <cbbcbail@gmail.com>
2015-02-13 14:21:14 -0700

    Update popManger.java

--- src/eco/popManger.java
+++ src/eco/popManger.java
@@ -1,67 +1,65 @@
 package eco;
 
 public class popManger {
+    
 	public static int wPopulation = 0;
 	public static int fPopulation = 0;
 	public static int tPop = 0;
 
 	public static void initpops() {
-				for(int i =0; i < Main.popArray.length; i++){
-						Main.popArray[i] = new Pops();
-				}
-		Main.popArray[0].people=5;
-		Main.popArray[1].people=5;
+        for(int i =0; i < Main.popArray.length; i++){
+            Main.popArray[i] = new Pops();
+        }
+        
+		Main.popArray[0].people = 5;
+		Main.popArray[1].people = 5;
 		Main.popArray[0].isFarmer = true;
 		Main.popArray[0].acres = 5;
 		Main.popArray[1].isWarrior = true;
 		Main.popArray[0].isUsed = true;
 		Main.popArray[1].isUsed = true;
-			//	System.out.println("Shieeet");
+        
 	}
 
-	public static void popController(){
+	public static void popController() {
+        
 		int x = 0;
 		int y= 0;
-				int r = 0;
-				int q = 0;
-				int w = 0;
-				int k = 0;
-				int m = 0;
-				int e = 0;
-				int newtPop = 0;
-				int oldtPop;
+        int r = 0;
+        int q = 0;
+        int w = 0;
+        int k = 0;
+        int m = 0;
+        int e = 0;
+        int newtPop = 0;
+        int oldtPop;
+        
+        popMethods.scanPops();
+		
+		System.out.println(popMethods.usedacres());
+        Main.unusedacres = Main.tAcres - popMethods.usedacres();
+	
+        popMethods.unusedAcresFarmersAssignment();
 
-	popMethods.scanPops();
-				System.out.println(popMethods.usedacres());
-				Main.unusedacres = Main.tAcres - popMethods.usedacres();
-	popMethods.unusedAcresFarmersAssignment();
+        wPopulation = popMethods.warriortotal();
+        fPopulation = popMethods.farmertotal();
 
-				wPopulation = popMethods.warriortotal();
-				fPopulation = popMethods.farmertotal();
-			//	System.out.println(wPopulation);
-		//		System.out.println(fPopulation);
-				q = Warrior.wPop(wPopulation);
-				r = Farmer.fPop(fPopulation);
-			//	System.out.println(q);
-			//	System.out.println(r);
-						//System.out.println("feS");
-				oldtPop = wPopulation + fPopulation;
-				newtPop = q +r;
-				Main.unusedpops = newtPop- oldtPop;
-				System.out.println(Main.unusedpops);
-				popMethods.popAssigner();
-			//	System.out.println("sfesf");
-				popMethods.popBuilder(1);
-				wPopulation = q;
-				fPopulation = r;
-				tPop = wPopulation + fPopulation;
-			//	System.out.println("here");
-				popMethods.farmerconsumecycle();
-				popMethods.consumecyclewarrior();
+        q = Warrior.wPop(wPopulation);
+        r = Farmer.fPop(fPopulation);
 
+        oldtPop = wPopulation + fPopulation;
+        newtPop = q +r;
+        Main.unusedpops = newtPop- oldtPop;
+        System.out.println(Main.unusedpops);
+        popMethods.popAssigner();
 
+        popMethods.popBuilder(1);
+        wPopulation = q;
+        fPopulation = r;
+        tPop = wPopulation + fPopulation;
+
+        popMethods.farmerconsumecycle();
+        popMethods.consumecyclewarrior();
 
 	}
-
-
 }
