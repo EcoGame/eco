8d7a6b08e9285063470635f240effecc92356166
Retro-Frost <supacow101@gmail.com>
2015-02-19 14:30:37 -0700

    Update Farmer.java
    Starvation method

--- src/eco/Farmer.java
+++ src/eco/Farmer.java
@@ -9,6 +9,7 @@ public class Farmer {
     public static int fHunger(int p){
         int x = 0;
         x = p*11;
+        
         return x;
     }
     
@@ -25,5 +26,21 @@ public class Farmer {
         return k;
         
     }
+    public static int checkStarvation(int hungriness, int wheat ){
+        int deadpeople;
+        int q;
+        int e;
+        double k;
+        double o;
+        if(hungriness > wheat){
+            o = wheat/11;
+            k = hungriness/11;
+            e = (int) Math.floor(k);// floors hungirness
+            q = (int) Math.floor(o);// floors total wheat
+            deadpeople = e-q;
+            return deadpeople;
+        }
+        return 0;
+    } 
     
 }
