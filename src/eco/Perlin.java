dc2dee4add12d0cf76400b1618033311791e1385
Phillip <phillipheikoop@gmail.com>
2015-02-20 14:07:23 -0700

    Update Perlin.java

--- src/eco/Perlin.java
+++ src/eco/Perlin.java
@@ -1,228 +1,174 @@
 package eco;
 
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
+/*
+* This file is part of 3DzzD http://dzzd.net/.
+*
+* Released under LGPL
+*
+* 3DzzD is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* 3DzzD is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with 3DzzD.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2005 - 20010 Bruno Augier
+*/
 
-import javax.imageio.ImageIO;
 
-public class Perlin {
+/**
+ * Fast perlin noise generation
+ *
+ * Generate a perlin noise with 8 octave and a persistence of 0.5
+ *
+ * NB: 
+ * - output range between 0 and 255
+ * - maximum octave = 7
+ * 
+ * you can change type of noise between grad & value noise by commenting/uncommenting block
+ * you can change type of interpolation between bicubic/bilinear by commenting/uncommenting block
+ */
+public class Perlin
+{
+   public static int noise(double x,double y,int nbOctave)
+   {
+      int result=0;      
+      int frequence256=256; 
+      int sx=(int)((x)*frequence256); 
+      int sy=(int)((y)*frequence256); 
+      int octave=nbOctave;   
+      while(octave!=0) 
+      {
+         int bX=sx&0xFF;
+         int bY=sy&0xFF;
 
-	public static int maxheight = 1;
-	public static int octaveCount = 7;
-	public static int stoneheight =  2 * maxheight / 3;
-	public static int heightGradients = 1;
-    public static int width = 128;
-	public static int height = 128;
-	public static int size = 7;
-	public static boolean applyHeightMask = true;
-	
-	public static  Random random = new Random();
-	
-	public static void main(String[] args) {
-        
-		float[][] noise = newWorld(size);
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        for(int y = 0; y < height; y++){
-            
-            for(int x = 0; x < width; x++){
-                
-                float c = noise[x][y];
-                c *= 128.0;
-                c += 127.0;
-                if(c > 255.0){
-                    c = 255.0f;
-                }
-                if(c < 0.0){
-                    c = 0.0f;
-                }
-                int r = (int) c;
-                int g = (int) c;
-                int b = (int) c;
-                int color = new Color(r, g, b).getRGB();
-                image.setRGB(x, y, color);
-                
-            }
-	        
-        }
-        File fileImage = new File("noise.png");
-        try {
-            ImageIO.write(image, "png", fileImage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-	}
-	
-	public static void setSeed(long seed) {
-		random.setSeed(seed);
-	}
-	
-	public static void setMaxHeight(int height) {
-		maxheight = height;
-	}
-	
-	public static void setSeaLevel(int sealevel) {
-		
-	}
-	
-	public static float[][] newWorld(int size) {
-		//double[][] noisebase = NoiseGen.getMap(size);
-		
-		int[][] noisebase = new int[(int) Math.pow(2, size)][(int) Math.pow(2, size)];
-		float[][] heightbase = new float[(int) Math.pow(2, size)][(int) Math.pow(2, size)];
-		int mapsize = (int) Math.pow(2, size);
-		float[][] gradients = new float[mapsize][mapsize];
-		
-		for (int x = 0; x < mapsize; x++) {
-			for (int y = 0; y < mapsize; y++) {
-				gradients[x][y] = maxheight;
-			}
-		}
-		
-		for (int i = 0; i < heightGradients; i++) {
-			gradients = addGradient(gradients, mapsize);
-		}
-		
-		random.setSeed(System.currentTimeMillis());
-		float[][][] noiseOctaves = new float[octaveCount][mapsize][mapsize];
-		
-		for (int i = 0; i < octaveCount; i++) {
-			noiseOctaves[i] = GenerateSmoothNoise(genRandomNoise(mapsize), i);
-		} 
-		
-		float[][] perlinNoise = new float[mapsize][mapsize];
-        
-		for (int i = 0; i < heightbase.length; i++) {
-			for (int k = 0; k < heightbase.length; k++) {
-				perlinNoise[i][k] = 0;
-			}
-		}
-		
-	    float amplitude = 5.5f;
-	    float totalAmplitude = 0.0f;
-	    float persistance = 0.525f;
-	 
-	    for (int octave = octaveCount - 1; octave >= 0; octave--) {
-	       amplitude *= persistance;
-	       totalAmplitude += amplitude;
-	 
-	       for (int i = 0; i < mapsize; i++) {
-	          for (int j = 0; j < mapsize; j++) {
-	             perlinNoise[i][j] += (noiseOctaves[octave][i][j] * amplitude);
-	          }
-	       }
-        }
-        
-	   for (int i = 0; i < mapsize; i++) {
-	      for (int j = 0; j < mapsize; j++) {
-	         perlinNoise[i][j] /= totalAmplitude;
-	      }	
-       }
+         int sxp=sx>>8;
+         int syp=sy>>8;
+         
 
-        for (int i = 0; i < noisebase.length; i++) {
-            
-			for (int k = 0; k < noisebase.length; k++) {
-                
-				if (perlinNoise[i][k] >= maxheight) {
-					perlinNoise[i][k] = (float) (maxheight - 0.01);
-				}
-				if (applyHeightMask) {
-					perlinNoise[i][k] -= gradients[i][k];
-				}
-				perlinNoise[i][k] *= 128;
-				perlinNoise[i][k] += 128;
+         //Compute noise for each corner of current cell
+         int Y1376312589_00=syp*1376312589;
+         int Y1376312589_01=Y1376312589_00+1376312589;
 
-			}
-		}
-		return perlinNoise;
-	}
-	
-	public static float[][] genRandomNoise(int size) {
-		float[][] result = new float[size][size];
-		for (int i = 0; i < size; i++) {
-			for (int k = 0; k < size; k++) {
-				result[i][k] = random.nextFloat() - random.nextInt(2);
-			}
-		}
-		return result;
-	}
-	
-	static float[][] GenerateSmoothNoise(float[][] baseNoise, int octave) {
-	   int width = baseNoise.length;
-	   int height = baseNoise[0].length;
-	 
-	   float[][] smoothNoise = new float[width][height];
-	 
-	   int samplePeriod = (int) Math.pow(2, octave);
-	   float sampleFrequency = 1.0f / samplePeriod;
-	 
-	   for (int i = 0; i < width; i++) {
-	      int sample_i0 = (i / samplePeriod) * samplePeriod;
-	      int sample_i1 = (sample_i0 + samplePeriod) % width; 
-	      float horizontal_blend = (i - sample_i0) * sampleFrequency;
-	 
-	      for (int j = 0; j < height; j++) {
-	         int sample_j0 = (j / samplePeriod) * samplePeriod;
-	         int sample_j1 = (sample_j0 + samplePeriod) % height;
-	         float vertical_blend = (j - sample_j0) * sampleFrequency;
-	         float top = Interpolate(baseNoise[sample_i0][sample_j0], baseNoise[sample_i1][sample_j0], horizontal_blend);
-	         float bottom = Interpolate(baseNoise[sample_i0][sample_j1], baseNoise[sample_i1][sample_j1], horizontal_blend);
-	         smoothNoise[i][j] = Interpolate(top, bottom, vertical_blend);
-	      }
-	   }
-	   return smoothNoise;
-	}
-	
-	static float Interpolate(float x0, float x1, float alpha) {
-	   return x0 * (1 - alpha) + alpha * x1;
-	}
-	
-	public static float[][] addGradient(float[][] map, int radius) {
-		float xpos = (float) ((float) (map.length / 2));
-		float ypos = (float) ((float) (map.length / 2));
-		
-		for (int x = 0; x < map.length; x++) {
-			for (int y = 0; y < map.length; y++) {
-		        float distanceX = (xpos - x) * (xpos - x);
-		        float distanceY = (ypos - y) * (ypos - y);
-		        float distanceToCenter = (float) Math.sqrt(distanceX + distanceY);
-		        
-		        if (distanceToCenter <= radius) {
-		        	distanceToCenter = (distanceToCenter) / (128.0f);
-		        	if (map[x][y] == maxheight) {
-                        map[x][y] = distanceToCenter;
-                    }
-		        	else {
-		        		map[x][y] = (distanceToCenter + map[x][y]) / 2f;
-		        	}
-		        }
-			}
-		}
-		return map;
-	}
-	
-	public static float findGreatest(float[][] map) {
-        
-		float greatest = 0.0f;
-        
-		for (int x = 0; x < map.length; x++) {
-			for (int y = 0; y < map[0].length; y++) {
-				greatest = Math.max(greatest, map[x][y]);
-			}
-		}
-		return greatest;
-	}
-	
-	public static void printNoise(float[][] map) {
-        
-		for (int i = 0; i < map.length; i++) {
-			for (int k = 0; k < map.length; k++) {
-				System.out.print(map[i][k]);
-				System.out.print(",");
-			}
-			System.out.println();
-		}
-	}
+         int XY1376312589_00=sxp+Y1376312589_00;
+         int XY1376312589_10=XY1376312589_00+1;
+         int XY1376312589_01=sxp+Y1376312589_01;
+         int XY1376312589_11=XY1376312589_01+1;
+
+         int XYBASE_00=(XY1376312589_00<<13)^XY1376312589_00;
+         int XYBASE_10=(XY1376312589_10<<13)^XY1376312589_10;
+         int XYBASE_01=(XY1376312589_01<<13)^XY1376312589_01;
+         int XYBASE_11=(XY1376312589_11<<13)^XY1376312589_11;
+
+         int alt1=(XYBASE_00 * (XYBASE_00 * XYBASE_00 * 15731 + 789221) + 1376312589) ;
+         int alt2=(XYBASE_10 * (XYBASE_10 * XYBASE_10 * 15731 + 789221) + 1376312589) ;
+         int alt3=(XYBASE_01 * (XYBASE_01 * XYBASE_01 * 15731 + 789221) + 1376312589) ;
+         int alt4=(XYBASE_11 * (XYBASE_11 * XYBASE_11 * 15731 + 789221) + 1376312589) ;
+         
+         /*
+          *NOTE : on  for true grandiant noise uncomment following block
+          * for true gradiant we need to perform scalar product here, gradiant vector are created/deducted using
+          * the above pseudo random values (alt1...alt4) : by cutting thoses values in twice values to get for each a fixed x,y vector 
+          * gradX1= alt1&0xFF 
+          * gradY1= (alt1&0xFF00)>>8
+          *
+          * the last part of the PRN (alt1&0xFF0000)>>8 is used as an offset to correct one of the gradiant problem wich is zero on cell edge
+          *
+          * source vector (sXN;sYN) for scalar product are computed using (bX,bY)
+          *
+          * each four values  must be replaced by the result of the following 
+          * altN=(gradXN;gradYN) scalar (sXN;sYN)
+          *
+          * all the rest of the code (interpolation+accumulation) is identical for value & gradiant noise
+          */
+          
+          
+         /*START BLOCK FOR TRUE GRADIANT NOISE*/
+         
+          /*int grad1X=(alt1&0xFF)-128;
+          int grad1Y=((alt1>>8)&0xFF)-128;
+          int grad2X=(alt2&0xFF)-128;
+          int grad2Y=((alt2>>8)&0xFF)-128;
+          int grad3X=(alt3&0xFF)-128;
+          int grad3Y=((alt3>>8)&0xFF)-128;
+          int grad4X=(alt4&0xFF)-128;
+          int grad4Y=((alt4>>8)&0xFF)-128;
+          
+          
+          int sX1=bX>>1;
+          int sY1=bY>>1;
+          int sX2=128-sX1;
+          int sY2=sY1;
+          int sX3=sX1;
+          int sY3=128-sY1;
+          int sX4=128-sX1;
+          int sY4=128-sY1;
+          alt1=(grad1X*sX1+grad1Y*sY1)+16384+((alt1&0xFF0000)>>9); //to avoid seams to be 0 we use an offset
+          alt2=(grad2X*sX2+grad2Y*sY2)+16384+((alt2&0xFF0000)>>9);
+          alt3=(grad3X*sX3+grad3Y*sY3)+16384+((alt3&0xFF0000)>>9);
+          alt4=(grad4X*sX4+grad4Y*sY4)+16384+((alt4&0xFF0000)>>9);*/
+          
+         /*END BLOCK FOR TRUE GRADIANT NOISE */
+         
+         
+         /*START BLOCK FOR VALUE NOISE*/
+         
+          alt1&=0xFFFF;
+          alt2&=0xFFFF;
+          alt3&=0xFFFF;
+          alt4&=0xFFFF;
+          
+         /*END BLOCK FOR VALUE NOISE*/
+         
+         
+         /*START BLOCK FOR LINEAR INTERPOLATION*/
+         //BiLinear interpolation 
+         /*
+         int f24=(bX*bY)>>8;
+         int f23=bX-f24;
+         int f14=bY-f24;
+         int f13=256-f14-f23-f24;
+
+         int val=(alt1*f13+alt2*f23+alt3*f14+alt4*f24);
+         */
+         /*END BLOCK FOR LINEAR INTERPOLATION*/
+         
+         
+         
+         //BiCubic interpolation ( in the form alt(bX) = alt[n] - (3*bX^2 - 2*bX^3) * (alt[n] - alt[n+1]) )
+         /*START BLOCK FOR BICUBIC INTERPOLATION*/
+         int bX2=(bX*bX)>>8;
+         int bX3=(bX2*bX)>>8;
+         int _3bX2=3*bX2;
+         int _2bX3=2*bX3;
+         int alt12= alt1 - (((_3bX2 - _2bX3) * (alt1-alt2)) >> 8);
+         int alt34= alt3 - (((_3bX2 - _2bX3) * (alt3-alt4)) >> 8);
+         
+         
+         int bY2=(bY*bY)>>8;
+         int bY3=(bY2*bY)>>8;
+         int _3bY2=3*bY2;
+         int _2bY3=2*bY3;
+         int val= alt12 - (((_3bY2 - _2bY3) * (alt12-alt34)) >> 8);
+         
+         val*=256;
+         /*END BLOCK FOR BICUBIC INTERPOLATION*/
+         
+         
+         //Accumulate in result
+         result+=(val<<octave);
+         
+         octave--;
+         sx<<=1; 
+         sy<<=1;
+         
+      }
+      return result>>>(16+nbOctave+1);   
+   }
 }
