db9ab1ccb870893627bb56eb2804d4d24f38f4e0
Phillip <phillipheikoop@gmail.com>
2015-02-20 14:08:42 -0700

    Update UIManager.java

--- src/eco/UIManager.java
+++ src/eco/UIManager.java
@@ -1,13 +1,13 @@
 package eco;
 
-import java.util.ArrayList;
 
 public class UIManager{
-	public static Button testButton = new Button(100, 10, 50, 2, 2);
+	public static Button testButton = new Button(800, 100, 50, 1, 2);
 
 	public static void update(){
 		if (testButton.checkForClick()){
-			World.messages.add(new Message("dank buttons", 10, 30, 30));
+			//World.messages.add(new Message("dank buttons", 10, 30, 30));
+			Render.multithreading ^= true;
 		}
 	}
 
