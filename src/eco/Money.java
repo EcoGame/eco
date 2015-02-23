200f03883cececbd752c42a1e57807b9fab70434
Nate <4u2njoy.nathan@gmail.com>
2015-02-23 14:24:01 -0700

    Added GDP

--- src/eco/Money.java
+++ src/eco/Money.java
@@ -3,12 +3,18 @@ package eco;
 public class Money {
     
     public static int tMoney;
+    public static int GDP;
     
     public static int tMoney(int tWheat, int wheatPrice){
         
         tMoney = (int)(tMoney + ((tWheat - Main.aggDemand) * wheatPrice));
         return tMoney;
         
+    }
+    
+    public static int GDP(int tWheat, int wheatPrice){
+    	GDP = tWheat * wheatPrice;
+    	return GDP;
     }
     
 }
