9f3aac6484f607eac7bc266aeaf8634df5b4da4e
Connor <cbbcbail@gmail.com>
2015-02-24 14:13:45 -0700

    Update InputManager.java

--- src/eco/InputManager.java
+++ src/eco/InputManager.java
@@ -47,6 +47,7 @@ public class InputManager {
 										-4f, World.mapsize / 2f * Render.tilesize);
 						System.out.println("Reset camera to default location");
 						break;
+					case Keyboard.H:
 					case Keyboard.KEY_GRAVE:
 						Main.fullDebug ^= true;
 						break;
