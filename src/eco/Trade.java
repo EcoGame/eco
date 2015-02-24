6d365b995e036ae5e490b3565b38bde3951ee57f
Nate <4u2njoy.nathan@gmail.com>
2015-02-24 14:28:22 -0700

    Update Trade.java

--- src/eco/Trade.java
+++ src/eco/Trade.java
@@ -7,6 +7,7 @@ public class Trade {
         stockPile = stockPile + tWheat;
         //I know for a fact that this is not what Will wants
         //so it's for sure crap and needs to be changed.
+        return stockPile;
         }
     
 
