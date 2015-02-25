2f911063d74a4d85664fd06d008673a9e020c5d4
Nate <4u2njoy.nathan@gmail.com>
2015-02-25 13:54:21 -0700

    Update Tax.java

--- src/eco/Tax.java
+++ src/eco/Tax.java
@@ -6,7 +6,7 @@ public class Tax {
     public static int taxRevenue(int countrycode){
          for(int i=0; i < Main.popArray[countrycode].length; i++){
          	taxRevenue = taxRevenue + Main.popArray[countrycode][i].groupmoney;
-         	System.out.println("Tax: " + taxRevenue);
+         	//System.out.println("Tax: " + taxRevenue);
         }
         return taxRevenue;
 	}
