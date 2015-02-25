a74bb21ec1915b811918c5a79fef8f6c208765f6
Nate <4u2njoy.nathan@gmail.com>
2015-02-25 14:29:34 -0700

    Update Tax.java

--- src/eco/Tax.java
+++ src/eco/Tax.java
@@ -2,14 +2,37 @@ package eco;
 
 public class Tax {
 	public static int taxRevenue;
+	public static int[] tax = new int[75];
+	public static int taxRate = 20;
 	
     public static int taxRevenue(int countrycode){
-         for(int i=0; i < Main.popArray[countrycode].length; i++){
-         	taxRevenue = taxRevenue + Main.popArray[countrycode][i].groupmoney;
-         	//System.out.println("Tax: " + taxRevenue);
+        for(int i=0; i < Main.popArray[countrycode].length; i++){
+         	taxRevenue = taxRevenue + Main.popArray[countrycode][i].groupmoney * taxRate;
+         	int x = Main.popArray[countrycode][i].groupmoney;
+         	Main.popArray[countrycode][i].groupmoney = (x - (x * taxRate));
         }
-        return taxRevenue;
-	}
-    
+        int y = 620;
+        int x = 854;
+        int shift = 100;
+        
+    	if(Main.debug){
+    		System.out.println("Tax: " + taxRevenue);
+    	}
+        tax[74] = taxRevenue;
+        
+        
+    	for(int i=0; i<tax.length; i++){
+    		World.messages.add(new Message("\u25A0 .", (x - 160 - shift) + i, (y - shift) - (tax[i]), 1));
+    	}
 
+
+    	for(int i=0; i<tax.length - 1; i++){
+        	tax[i] = tax[i + 1];
+    	}
+    	
+    	World.messages.add(new Message("Tax", (x - 160 - shift), y - 60, 1));
+    	return taxRevenue;
+	}	
+    
 }
+
