bd1fc7f4a44fe7114b330723d5579b7f5062bd66
Connor Bailey <cbbcbail@gmail.com>
2015-02-28 12:49:17 -0700

    Render link no longer needed
    Main refers to the Rendering files directly as opposed to through this
    link.

--- src/eco/Main.java
+++ src/eco/Main.java
@@ -4,6 +4,8 @@ import java.util.Random;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.SharedDrawable;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
 
 public class Main {
 
@@ -42,9 +44,9 @@ public class Main {
 		System.out.println("Welcome to EcoLand!");
 		gameLoop();
 		System.exit(0);
-		
+
 	}
-	
+
 	public static void gameLoop(){
 		init();
 		while (year < ticks && !Display.isCloseRequested()){
@@ -61,9 +63,52 @@ public class Main {
 			}
 			UIManager.update();
 			InputManager.update();
-			tAcres = RenderLink.tick(year);
+			tAcres = 0;
+			if (!Main.paused){
+				Render.draw();
+				Render.initOrtho();
+		    Render.drawString("FPS: " + String.valueOf(FPSCounter.getFPS()), 10, 10);
+		    Render.drawString("Year: " + String.valueOf(year), 10, 30);
+    		if (Render.multithreading) {
+						Render.drawString("Multithreading On", Main.width - (Render.font.getWidth("Multithreading On") + 5), 30);
+		    		Render.drawString("Using Bufferobjects", Main.width - (Render.font.getWidth("Using Bufferobjects") + 5), 10);
+		    }
+		   	else{
+		    		Render.drawString("Using Immediate Mode :(", Main.width - (Render.font.getWidth("Using Immediate Mode :(") + 5), 10);
+		    }
+		    if (Main.fullDebug){
+					int tPop = PopManager.fPopulation + PopManager.wPopulation;
+		     	Render.drawString("Wheat Produced: " + String.valueOf(PopManager.uneatenWheat), 10, 70);
+         	Render.drawString("Unused Acres: " + String.valueOf(PopManager.unusedAcres), 10, 90);
+		      Render.drawString("Wheat Price: " + String.valueOf(Main.wheatPrice), 10, 110);
+	        Render.drawString("Money in Treasury: " + String.valueOf(Money.tMoney), 10, 130);
+	        Render.drawString("Farmers: " + String.valueOf(Farmer.fPop), 10, 150);
+	        Render.drawString("Employed Farmers: " + String.valueOf(Wheat.employedFarmers), 10, 170);
+	        Render.drawString("Unemployed Farmers: " + String.valueOf(Wheat.unemployedFarmers), 10, 190);
+		      if (Wheat.employedFarmers + Wheat.unemployedFarmers != PopManager.fPopulation){
+		     		Render.drawString("Warning! Population is mis-matched!", 100, 210);
+		      }
+		      Render.drawString("Total Population: " + String.valueOf(tPop), 10, 230);
+		      Render.drawString("Warriors: " + String.valueOf(PopManager.wPopulation), 10, 250);
+		      Render.drawString("Unassigned: " + String.valueOf(PopManager.unusedPops), 10, 270);
+				}
+		    FPSCounter.tick();
+        Display.update();
+	      Display.sync(60);
+			}
+			else{
+				Render.drawPaused();
+		    FPSCounter.tick();
+	     	Display.update();
+		    Display.sync(60);
+			}
 		}
-		RenderLink.simDone();
+		while (!Keyboard.isKeyDown(Keyboard.KEY_ESCAPE) && !Display.isCloseRequested()) {
+			Render.draw();
+			InputManager.update();
+			Display.update();
+			Display.sync(60);
+		}
 		//****************//
 		//     WARNING    //
 		//                //
@@ -95,9 +140,9 @@ public class Main {
         ThreadManager.addJob(new MeshTask());
 
 	}
-	
+
 	public static void willTick(){
-        
+
 		PopManager.popController(tAcres, 0);
 		int farmPacks = Wheat.farmPacks(tAcres);
 		int unemployedFarmers = Wheat.unemployedFarmers(farmPacks, PopManager.fPopulation);
