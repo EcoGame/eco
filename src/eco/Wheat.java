4554708a8a1c9d5de2c3efe40897698b0aba4285
Phillip <phillipheikoop@gmail.com>
2015-03-03 14:22:52 -0700

    Update Wheat.java

--- src/eco/Wheat.java
+++ src/eco/Wheat.java
@@ -4,40 +4,42 @@ public class Wheat {
 
     public static int tWheat;
     public static int wheatPrice;
-    public static int unemployedFarmers = 0;
-    public static int employedFarmers = 0;
+    
+    public static int wheatPerFarmer = 50;
+   
+   
+   /**/ public static int unemployedFarmers = 0;
+   /**/  public static int employedFarmers = 0;
 
-    public static int farmPacks(int tAcres) {
+   /**/  public static int farmPacks(int tAcres) {
 
-        int farmPacks = tAcres/5;
-        return farmPacks;
+       /**/  int farmPacks = tAcres/5;
+        /**/ return farmPacks;
 
-    }
+     /**/}
 
-    public static int unemployedFarmers(int farmPacks, int fPop) {
+    /**/ public static int unemployedFarmers(int farmPacks, int fPop) {
 
-        unemployedFarmers = fPop - farmPacks;
+       /**/  unemployedFarmers = fPop - farmPacks;
 
-        if(unemployedFarmers < 0) {
-            unemployedFarmers = 0;
-        }
+         /**/if(unemployedFarmers < 0) {
+          /**/   unemployedFarmers = 0;
+         /**/}
 
-        return unemployedFarmers;
+         /**/return unemployedFarmers;
 
-    }
+    /**/ }
 
-    public static int employedFarmers(int fPop, int unemployedFarmers) {
+    /**/ public static int employedFarmers(int fPop, int unemployedFarmers) {
 
-        employedFarmers = (fPop - unemployedFarmers);
-        return employedFarmers;
+         /**/employedFarmers = (fPop - unemployedFarmers);
+        /**/ return employedFarmers;
 
-    }
+     /**/}
 
-    public static int tWheat(int employedFarmers) {
-
-        tWheat = employedFarmers * 40;
+    public static int tWheat(int farmers) {
+        tWheat = farmers * wheatPerFarmer;
         return tWheat;
-
     }
 
 }
