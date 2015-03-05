c6c53848bdff227742dde961441e6656552e0fdf
Nate <4u2njoy.nathan@gmail.com>
2015-03-05 14:53:44 -0700

    Removed willTick references

--- src/eco/Tax.java
+++ src/eco/Tax.java
@@ -1,36 +1,11 @@
 package eco; 
 
 public class Tax {
-	public static int taxRevenue;
-	public static int[] tax = new int[75];
+	public static int taxRevenue = 500;
 	public static int taxRate = 20;
 
-    public static int taxRevenue(int countrycode){
-        for(int i=0; i < Main.popArray[countrycode].length; i++){
-         	taxRevenue = taxRevenue + Main.popArray[countrycode][i].groupmoney * taxRate;
-         	int x = Main.popArray[countrycode][i].groupmoney;
-         	Main.popArray[countrycode][i].groupmoney = (x - (x * taxRate));
-        }
-        int y = 620;
-        int x = 854;
-        int shift = 100;
-
-    	if(Main.debug){
-    		System.out.println("Tax: " + taxRevenue);
-    	}
-        tax[74] = taxRevenue;
-
-
-    	for(int i=0; i<tax.length; i++){
-    		World.messages.add(new Message("\u25A0 .", (x - 160 - shift) + i, (y - shift) - (tax[i]), 1));
-    	}
-
-
-    	for(int i=0; i<tax.length - 1; i++){
-        	tax[i] = tax[i + 1];
-    	}
-
-    	World.messages.add(new Message("Tax", (x - 160 - shift), y - 60, 1));
+    public static int taxRevenue(){
+		taxRevenue = taxRevenue - Util.randInt(-2,6);
     	return taxRevenue;
 	}
 
