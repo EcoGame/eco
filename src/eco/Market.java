635f16cf9fa1d35945d524901bd7bb95dd10901b
Nate <4u2njoy.nathan@gmail.com>
2015-03-05 14:21:01 -0700

    Took out graph code

--- src/eco/Market.java
+++ src/eco/Market.java
@@ -6,7 +6,7 @@ public class Market {
 	static int[] year = new int[75];
     public static int wheatPrice(int pastPrice){
         int newPrice = 1;
-        int chance = (Util.randInt(0,100));
+        int chance = (Util.randInt(0, 100));
         if(chance > 50){
             newPrice = pastPrice + Util.randInt(1,5);
         }
@@ -17,52 +17,6 @@ public class Market {
                 //World.messages.add(new Message("Price is one!", 100, 100, 300));
             }
         }
-        //int num = 100000000;
-        int y = 620;
-        int x = 854;
-        int shift = 100;
-        //Render.awtFont = Render.awtFont.deriveFont(24f); // set font size
-		//Render.font = new Render.TrueTypeFont(Render.awtFont, false);
-
-        prices[74] = newPrice;
-        //if(Main.year % 35 == 0){
-        //	year[74] = Main.year;
-        //}
-
-        for(int i=0; i<prices.length; i++){
-            	World.messages.add(new Message("\u25A0 .", (x - shift) + i, (y - shift) - (prices[i]), 1));
-            	//if(year[i] != 0)
-            	//	World.messages.add(new Message("" + year[i], (x - shift) + i, (y - shift), 1));
-         }
-
-
-    	for(int i=0; i<prices.length - 1; i++){
-            prices[i] = prices[i + 1];
-        }
-
-        World.messages.add(new Message("Price", (x - shift), y - 60, 1));
-
-
-        //for(int i=0; i<year.length - 1; i++){
-          //  year[i] = year[i + 1];
-        //}
-
-
-        //World.messages.add(new Message("" + year, (x - shift), (y - shift - 40), 1));
-
-
-        pop[74] = PopManager.wPopulation + PopManager.fPopulation;
-        for(int i=0; i<pop.length; i++){
-            	World.messages.add(new Message("\u25A0 .", (x - 80 - shift) + i, (y - shift) - (pop[i]), 1));
-         }
-
-
-    	for(int i=0; i<pop.length - 1; i++){
-            pop[i] = pop[i + 1];
-        }
-
-        World.messages.add(new Message("Pop", (x - 80 - shift), y - 60, 1));
-
 
 		return newPrice;
 
