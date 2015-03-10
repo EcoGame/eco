d1bd5bddb409a5aa08fd7a7990a90e324f856f68
Phillip <phillipheikoop@gmail.com>
2015-03-10 13:46:36 -0600

    Update UIManager.java

--- src/eco/UIManager.java
+++ src/eco/UIManager.java
@@ -12,6 +12,12 @@ public class UIManager{
 	public static Button increaseTickRate = new Button(495, 606, 20, 0, 2, 1, 2);
 	public static Button decreaseTickRate = new Button(495, 636, 20, 2, 2, 3, 2);
 
+	public static TextButton startTempGame = new TextButton((Main.width / 2) - 128, 256, 256, 32, 6, 2, 7, 2, "Start Test Game");
+
+	public static ToggleTextButton generatorIsland = new ToggleTextButton((Main.width / 2) + 256, 256, 256, 32, 6, 2, 7, 2, "Island", true);
+	public static ToggleTextButton generatorArchipelago = new ToggleTextButton((Main.width / 2) + 256, 320, 256, 32, 6, 2, 7, 2, "Archipelago", false);
+	public static ToggleTextButton generatorMountains = new ToggleTextButton((Main.width / 2) + 256, 384, 256, 32, 6, 2, 7, 2, "Mountains (WIP)", false);
+
 	public static void update(){
 		if (toggleFeedDisplaced.checkForClick()){
 			Main.displacedEat ^= true;
@@ -38,6 +44,55 @@ public class UIManager{
 			if (Main.framesPerTick != 2){
 				Main.framesPerTick--;
 			}
+		}
+	}
+
+	public static void clickMenu(float x, float y){
+		startTempGame.click(x, y);
+		generatorIsland.click(x, y);
+		generatorArchipelago.click(x, y);
+		generatorMountains.click(x, y);
+	}
+
+	public static void renderMenu(){
+		startTempGame.render(Mouse.getX(), Main.height - Mouse.getY());
+		generatorIsland.render(Mouse.getX(), Main.height - Mouse.getY());
+		generatorArchipelago.render(Mouse.getX(), Main.height - Mouse.getY());
+		generatorMountains.render(Mouse.getX(), Main.height - Mouse.getY());
+	}
+
+	public static void renderMenu2(){
+		startTempGame.render2();
+		generatorIsland.render2();
+		generatorArchipelago.render2();
+		generatorMountains.render2();
+	}
+
+	public static void render2(){
+
+	}
+
+	public static void updateMenu(){
+		if (startTempGame.checkForClick()){
+			Main.initTempGame();
+			Main.shouldBeInMenu = false;
+		}
+		if (generatorIsland.checkForClick()){
+			Main.generatorToUse = 0;
+			generatorArchipelago.toggle = false;
+			generatorMountains.toggle = false;
+		}
+
+		if (generatorArchipelago.checkForClick()){
+			Main.generatorToUse = 1;
+			generatorIsland.toggle = false;
+			generatorMountains.toggle = false;
+		}
+
+		if (generatorMountains.checkForClick()){
+			Main.generatorToUse = 2;
+			generatorIsland.toggle = false;
+			generatorArchipelago.toggle = false;
 		}
 	}
 
