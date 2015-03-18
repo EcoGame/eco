3b4d0a80c99546b7816eae3cf83b15dda0ab91a4
Nate <4u2njoy.nathan@gmail.com>
2015-03-18 14:21:16 -0600

    Update compi.sh

--- compi.sh
+++ compi.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 reset
-echo Returning from the ashes to live another day…
-cd $(dirname "$0")
-echo Compiling...
-javac -cp .:lib/* src/**/*.java
-echo Compiled...
-cd src
+echo "Returning from the ashes to live another day…"
+#cd $(dirname "$0")
+if [ "$1" == "name" ];
+then echo "Compiling..."
+javac src/eco/NameGen.java
+echo "Compiled..."
 sleep 1
-echo Running…
+echo "Running..."
+java src/eco/NameGen.java
+else
+# Actual stuff below
+echo "Compiling..."
+javac -cp .:lib/* src/**/*.java
+echo "Compiled..."
+sleep 1
+echo "Running..."
 java -cp .:../lib/* -Djava.library.path=../native eco/Main
 cd eco
 rm -f *.class
-sleep 10
+fi
