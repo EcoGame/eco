3330ad1f90a17b15435796551e140817d9b3db79
Connor <cbbcbail@gmail.com>
2015-04-30 13:49:29 -0600

    Create GeneticMaster.java

---
+++ src/neural/GeneticMaster.java
@@ -0,0 +1,46 @@
+package neural;
+
+public class GeneticMaster{
+
+    public static void genemaster(){
+        
+        
+        
+        
+        
+    }
+ 
+    
+    public static void attributebreeder(){
+        
+        
+        
+        
+        
+    }
+    
+    public static void connectionbreeder(){
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    public static int calcfitness(){
+        
+        
+        
+        return 0;
+    }
+    
+    public static void selectparents(){
+        
+        
+        
+    }
+
+
+}
