c976496412798046e0d373d592493e292105aec4
Connor <cbbcbail@gmail.com>
2015-04-30 14:26:57 -0600

    Create Tuple.java

---
+++ src/eco/game/Tuple.java
@@ -0,0 +1,38 @@
+package eco.game;
+
+/**
+ * A small parameterized class for an object with two values, a 2-tuple
+ * 
+ * @author phil
+ * 
+ * @param <X>
+ * @param <Y>
+ */
+
+public class Tuple<X, Y> {
+
+	public X x;
+	public Y y;
+
+	public Tuple(X x, Y y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public X getX() {
+		return x;
+	}
+
+	public void setX(X x) {
+		this.x = x;
+	}
+
+	public Y getY() {
+		return y;
+	}
+
+	public void setY(Y y) {
+		this.y = y;
+	}
+
+}
