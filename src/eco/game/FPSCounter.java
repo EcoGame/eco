7ec6a14e8f29a4305153ad4c145c5a74ff1716ab
Connor <cbbcbail@gmail.com>
2015-04-30 14:34:57 -0600

    Create FPSCounter.java

---
+++ src/eco/game/FPSCounter.java
@@ -0,0 +1,30 @@
+package eco.game;
+
+/**
+ * A class that keeps track of how fast the program is running. Call
+ * <i>tick()</i> once every frame to update correctly.
+ * 
+ * @author phil
+ * 
+ */
+
+public class FPSCounter {
+
+	private static int totalFrames = 0;
+	private static int FPS = 0;
+	private static long delta = 0;
+
+	public static void tick() {
+		totalFrames++;
+		if (System.currentTimeMillis() - delta >= 1000) {
+			FPS = totalFrames;
+			totalFrames = 0;
+			delta = System.currentTimeMillis();
+		}
+	}
+
+	public static int getFPS() {
+		return FPS;
+	}
+
+}
