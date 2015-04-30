5286ed4ee232af9f267412c66733942e4e197f41
Connor <cbbcbail@gmail.com>
2015-04-30 13:52:00 -0600

    Create OutputNeuron.java

---
+++ src/neural/OutputNeuron.java
@@ -0,0 +1,34 @@
+package neural;
+
+public class OutputNeuron{
+
+public int firevalue = 2;
+public int currentvalue;
+public int fired;
+public int id;
+public int pairedaxon;
+public int action = 0;
+public boolean alreadyfired = false;
+
+public void addToCurrent(int x){
+
+currentvalue = x + currentvalue;
+    
+}
+public int firecheck(){
+ 
+if(currentvalue > firevalue){
+fired = 1;
+    //System.out.println("outputneuron " + id + " pairedaxon "+ pairedaxon + " current value "+ currentvalue);
+   
+    alreadyfired = true;
+    OutputActions.actions(action);
+}
+return fired;
+}
+public void reset(){
+currentvalue = 0;
+fired = 0;
+    alreadyfired = false;
+}
+}
