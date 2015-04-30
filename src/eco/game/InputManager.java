61c335e1f6e495efaf79cb3231712d1626af2164
Connor <cbbcbail@gmail.com>
2015-04-30 14:34:12 -0600

    Create InputManager.java

---
+++ src/eco/game/InputManager.java
@@ -0,0 +1,133 @@
+package eco.game;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+
+/**
+ * This class handles keyboard and mouse input, passing it along to where ever
+ * it needs to go
+ * 
+ * @author phil, connor
+ * 
+ */
+
+public class InputManager {
+
+	private static final float moveSpeed = 0.1f;
+
+	public static void update() {
+		if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
+			Render.rot -= 0.5f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_F)) {
+			Render.rot += 0.5f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			Render.camera.moveForward(moveSpeed);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			Render.camera.moveRight(moveSpeed);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			Render.camera.moveBack(moveSpeed);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			;
+			Render.camera.moveLeft(moveSpeed);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+			Render.camera.moveDown(moveSpeed);
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			Render.camera.moveUp(moveSpeed);
+		}
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				switch (Keyboard.getEventKey()) {
+				case Keyboard.KEY_G:
+					Main.debug ^= true;
+					System.out.println("DEBUG MODE IS TOGGLED!");
+					break;
+				case Keyboard.KEY_Q:
+					Render.camera = new Camera(-World.mapsize / 2f
+							* Render.tilesize, -4f, World.mapsize / 2f
+							* Render.tilesize);
+					System.out.println("Reset camera to default location");
+					break;
+				case Keyboard.KEY_H:
+				case Keyboard.KEY_GRAVE:
+					Main.fullDebug ^= true;
+					break;
+				case Keyboard.KEY_M:
+					Render.multithreading ^= true;
+					break;
+				case Keyboard.KEY_ESCAPE:
+					Main.paused ^= true;
+					break;
+				case Keyboard.KEY_F10:
+					Util.takeScreenshot();
+					break;
+				case Keyboard.KEY_O:
+					Render.overhead ^= true;
+					break;
+				}
+			}
+		}
+		while (Mouse.next()) {
+			if (Mouse.getEventButton() > -1) {
+				if (Mouse.getEventButtonState()) {
+					UIManager.click(Mouse.getX(),
+							Display.getHeight() - Mouse.getY());
+				} else {
+				}
+			}
+		}
+	}
+
+	public static void updateMenu() {
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				switch (Keyboard.getEventKey()) {
+				case Keyboard.KEY_F10:
+					Util.takeScreenshot();
+					break;
+				}
+			}
+		}
+		while (Mouse.next()) {
+			if (Mouse.getEventButton() > -1) {
+				if (Mouse.getEventButtonState()) {
+					UIManager.clickMenu(Mouse.getX(), Display.getHeight()
+							- Mouse.getY());
+				} else {
+				}
+			}
+		}
+	}
+
+	public static void updatePause() {
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				switch (Keyboard.getEventKey()) {
+				case Keyboard.KEY_F10:
+					Util.takeScreenshot();
+					break;
+				case Keyboard.KEY_ESCAPE:
+					Main.paused ^= true;
+					break;
+				}
+			}
+		}
+		while (Mouse.next()) {
+			if (Mouse.getEventButton() > -1) {
+				if (Mouse.getEventButtonState()) {
+					UIManager.clickPause(Mouse.getX(), Display.getHeight()
+							- Mouse.getY());
+				} else {
+				}
+			}
+		}
+	}
+
+}
