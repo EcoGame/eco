d721fb4e951c454bcf461ff78d54876f8c77cb3f
Connor <cbbcbail@gmail.com>
2015-04-30 13:51:45 -0600

    Create OutputActions.java

---
+++ src/neural/OutputActions.java
@@ -0,0 +1,21 @@
+package neural;
+
+  public class OutputActions{
+
+    public static void actions(int k){
+      switch(k){
+case 0:
+System.out.println("notta");
+
+break;
+
+case 1:
+System.out.println("taft");
+break;
+
+      }
+
+    }
+
+
+}
