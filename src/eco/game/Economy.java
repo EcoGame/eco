6fdce48a32ee49e858a5c1af39de287b6bec8923
Connor <cbbcbail@gmail.com>
2015-04-30 14:35:30 -0600

    Create Economy.java

---
+++ src/eco/game/Economy.java
@@ -0,0 +1,49 @@
+package eco.game;
+
+/**
+ * This class describes the international market for wheat, it manages wheat
+ * reserve goals and sells or buys to meet it.
+ * 
+ * @Author phil
+ */
+
+public class Economy {
+
+	private int treasury = 0;
+
+	private static int wheatPrice = 10;
+
+	public int buyWheat(int ammount) {
+		int neededMoney = wheatPrice * ammount;
+		if (neededMoney <= treasury) {
+			treasury -= neededMoney;
+			return ammount;
+		} else {
+			int canBuy = treasury / wheatPrice;
+			treasury = 0;
+			return canBuy;
+		}
+	}
+
+	public int sellWheat(int ammount) {
+		treasury += wheatPrice * ammount;
+		return ammount;
+	}
+
+	public void updateMarket(int time) {
+		wheatPrice += Util.randInt(-5, 5); // Biased to increase over time
+	}
+
+	public int getTreasury() {
+		return treasury;
+	}
+
+	public void setTreasury(int ammount) {
+		treasury = ammount;
+	}
+
+	public int getPrice() {
+		return wheatPrice;
+	}
+
+}
