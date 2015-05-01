941348f313293feaa9ff2713ba9d1f7a5d708113
Connor <cbbcbail@gmail.com>
2015-05-01 14:34:33 -0600

    Update NeuronReader.java

--- src/eco/neural/NeuronReader.java
+++ src/eco/neural/NeuronReader.java
@@ -4,11 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+
+/**
+ * This class does something
+ *
+ * @author will
+ *
+ */
+
 public class NeuronReader {
-	 public static int[][][][] axonArraytofill = new int[3][10][5][2];
+    
+    public static int[][][][] axonArraytofill = new int[3][10][5][2];
 	public static int[][][] neuronArrayfill = new int[3][10][2];
+    
 	public static void connectionreader(int num) {
-		 char code;
+        char code;
 		int type;
 		int axonid;
 		int filler;
@@ -20,64 +30,52 @@ public class NeuronReader {
 			String line;
 			while ((line = bufferedReader.readLine()) != null) {
 				code = line.charAt(0);
-
 				String[] tokenSpace = line.split(" ");
- 				
-			switch(code){
+                switch(code){
+                    case 'a':
+                        type = 0;
+                        axonid= Integer.parseInt(tokenSpace[1]);
 
-			case 'a':
-			type = 0;
-
- 			axonid= Integer.parseInt(tokenSpace[1]);
-
-			for(int x= 0; x < Main.axonArraytofill[type][axonid].length; x++){
-			for(int k = 0; k < Main.axonArraytofill[type][axonid][x].length; k++){
-				Main.axonArraytofill[type][axonid][x][k] = Integer.parseInt(tokenSpace[2+(x*2)+k]);
-                
-				}
-			}
-			break;
-			case 'b':
-			type = 1;
-
- 			axonid= Integer.parseInt(tokenSpace[1]);
-
-			for(int x= 0; x < Main.axonArraytofill[type][axonid].length; x++){
-			for(int k = 0; k < Main.axonArraytofill[type][axonid][x].length; k++){
-				Main.axonArraytofill[type][axonid][x][k] = Integer.parseInt(tokenSpace[2+(x*2)+k]);
-               
-
-				}
-			}
-			break;
-			case 'c':
-			type = 2;
-
- 			axonid= Integer.parseInt(tokenSpace[1]);
-
-			for(int x= 0; x < Main.axonArraytofill[type][axonid].length; x++){
-			for(int k = 0; k < Main.axonArraytofill[type][axonid][x].length; k++){
-				Main.axonArraytofill[type][axonid][x][k] = Integer.parseInt(tokenSpace[2+(x*2)+k]);
-               
-
-				}
-			}
-			break;
-
-					}
-			}
+                        for(int x= 0; x < Main.axonArraytofill[type][axonid].length; x++){
+                            for(int k = 0; k < Main.axonArraytofill[type][axonid][x].length; k++){
+                                Main.axonArraytofill[type][axonid][x][k] = Integer.parseInt(tokenSpace[2+(x*2)+k]);
+                            }
+                        }
+                        break;
+                    case 'b':
+                        type = 1;
+                        axonid= Integer.parseInt(tokenSpace[1]);
+                        for(int x= 0; x < Main.axonArraytofill[type][axonid].length; x++){
+                            for(int k = 0; k < Main.axonArraytofill[type][axonid][x].length; k++){
+                                Main.axonArraytofill[type][axonid][x][k] = Integer.parseInt(tokenSpace[2+(x*2)+k]);
+                            }
+                        }
+                        break;
+                    case 'c':
+                        type = 2;
+                        axonid= Integer.parseInt(tokenSpace[1]);
+                        for(int x= 0; x < Main.axonArraytofill[type][axonid].length; x++){
+                            for(int k = 0; k < Main.axonArraytofill[type][axonid][x].length; k++){
+                                Main.axonArraytofill[type][axonid][x][k] = Integer.parseInt(tokenSpace[2+(x*2)+k]);
+                            }
+                        }
+                        break;
+                }
+            }
 			fileReader.close();
-			
-		} catch (IOException e) {
+		}
+        catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-		public static void attributereader(int num) {
+    public static void attributereader(int num) {
+        
         char code;
 		int type;
 		int neuronid;
 		int filler;
+        
 		try {
 			File file = new File("./neural/neuron"+ num+".txt");
 			FileReader fileReader = new FileReader(file);
@@ -86,43 +84,34 @@ public class NeuronReader {
 			String line;
 			while ((line = bufferedReader.readLine()) != null) {
 				code = line.charAt(0);
-
-				
 				String[] tokenSpace = line.split(" ");
-
-			switch(code){
-
-			case 'a':
-			type = 0;
-
- 			neuronid= Integer.parseInt(tokenSpace[1])- Main.INPUTIDOFFSET;
-                   
-			for( int x = 0; Main.neuronArrayfill[type][neuronid].length > x; x++){
-			Main.neuronArrayfill[type][neuronid][x]= Integer.parseInt(tokenSpace[2+x]);
-			}
-			break;
-			case 'b':
-			type = 1;
-
- 			neuronid= Integer.parseInt(tokenSpace[1])- Main.OUTPUTIDOFFSET;
-			for( int x = 0; Main.neuronArrayfill[type][neuronid].length > x; x++){
-			Main.neuronArrayfill[type][neuronid][x]= Integer.parseInt(tokenSpace[2+x]);
-			}
-			break;
-			case 'c':
-			type = 2;
-
- 			neuronid= Integer.parseInt(tokenSpace[1])- Main.RELAYIDOFFSET;
-			for( int x = 0; Main.neuronArrayfill[type][neuronid].length > x; x++){
-			Main.neuronArrayfill[type][neuronid][x]= Integer.parseInt(tokenSpace[2+x]);
-			}
-			break;
-
-					}
+                switch(code){
+                    case 'a':
+                        type = 0;
+                        neuronid= Integer.parseInt(tokenSpace[1])- Main.INPUTIDOFFSET;
+                        for( int x = 0; Main.neuronArrayfill[type][neuronid].length > x; x++){
+                            Main.neuronArrayfill[type][neuronid][x]= Integer.parseInt(tokenSpace[2+x]);
+                        }
+                        break;
+                    case 'b':
+                        type = 1;
+                        neuronid= Integer.parseInt(tokenSpace[1])- Main.OUTPUTIDOFFSET;
+                        for( int x = 0; Main.neuronArrayfill[type][neuronid].length > x; x++){
+                            Main.neuronArrayfill[type][neuronid][x]= Integer.parseInt(tokenSpace[2+x]);
+                        }
+                        break;
+                    case 'c':
+                        type = 2;
+                        neuronid= Integer.parseInt(tokenSpace[1])- Main.RELAYIDOFFSET;
+                        for( int x = 0; Main.neuronArrayfill[type][neuronid].length > x; x++){
+                            Main.neuronArrayfill[type][neuronid][x]= Integer.parseInt(tokenSpace[2+x]);
+                        }
+                        break;
+                }
 			}
 			fileReader.close();
-			
-		} catch (IOException e) {
+		}
+        catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
