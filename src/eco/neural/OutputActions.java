51ff08c7b6dcecf4fc30040d422049207b77a9d4
Connor <cbbcbail@gmail.com>
2015-05-01 14:28:50 -0600

    Readability

--- src/eco/neural/OutputActions.java
+++ src/eco/neural/OutputActions.java
@@ -1,21 +1,24 @@
 package eco.neural;
 
-  public class OutputActions{
+/**
+ * This class does something
+ *
+ * @author will
+ *
+ */
+
+public class OutputActions{
 
     public static void actions(int k){
-      switch(k){
-case 0:
-System.out.println("notta");
-
-break;
-
-case 1:
-System.out.println("taft");
-break;
-
-      }
+        switch(k){
+            case 0:
+                System.out.println("notta");
+                break;
+            case 1:
+                System.out.println("taft");
+                break;
+        }
 
     }
-
 
 }
