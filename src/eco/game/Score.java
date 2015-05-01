97e4b32b47f46fc213c52280c21ecfa6e25171dd
Nate <4u2njoy.nathan@gmail.com>
2015-05-01 14:14:36 -0600

    Update Score.java

--- src/eco/game/Score.java
+++ src/eco/game/Score.java
@@ -1,1 +1,15 @@
 package eco.game;
+
+public class Score {
+    public static void calculateFitness(){
+        int score = Main.farmer.fPop() + Main.warrior.wPop() +
+        Main.wheat.gettWheat() +
+        Main.economy.getTreasury() +
+        System.out.println(score);
+        
+    }
+    
+    
+    
+    
+}
