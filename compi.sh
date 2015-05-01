8fc081aa5364eec777bbcd3bb14d852579219c0b
Connor <cbbcbail@gmail.com>
2015-05-01 14:13:35 -0600

    Update compi.sh

--- compi.sh
+++ compi.sh
@@ -1,16 +1,13 @@
+
 #!/bin/bash
 reset
-echo Returning from the ashes to live another day…
 cd $(dirname "$0")
 echo Compiling...
+javac -cp .:lib/* src/eco/game/*.java
+javac src/eco/neural/*.java
 echo Compiled...
-rm -r -f bin
-cp -r src/ bin/
-cp -r lib/ bin/lib
-javac -cp .:lib/* bin/**/*.java
-cd bin
+cd src
 echo Running…
-rm -f eco/*.java
-java -cp .:../lib/* -Djava.library.path=../native eco/Main
+java -cp .:../lib/* -Djava.library.path=../native eco/game/Main
 cd eco
 rm -f *.class
