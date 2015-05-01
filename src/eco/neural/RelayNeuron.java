890e973350f3058dd1fa1d1bc67df60c1f352b96
Connor <cbbcbail@gmail.com>
2015-05-01 14:25:20 -0600

    Update RelayNeuron.java

--- src/eco/neural/RelayNeuron.java
+++ src/eco/neural/RelayNeuron.java
@@ -1,28 +1,38 @@
 package eco.neural;
 
-public class RelayNeuron{
-public int firevalue =1;
-public int id;
-public int pairedaxon;
-public int fired = 0;
-public  int currentvalue;
+/**
+ * This class does something
+ *
+ * @author will
+ *
+ */
+
+public class RelayNeuron {
+    
+    public int firevalue =1;
+    public int id;
+    public int pairedaxon;
+    public int fired = 0;
+    public int currentvalue;
     public boolean alreadyfired = false;
 
-public void addToCurrent(int x){
+    public void addToCurrent(int x){
 
-currentvalue =  currentvalue + x;
-}
+        currentvalue =  currentvalue + x;
+    }
 
-public int firecheck(){
-if(currentvalue > firevalue){
-fired = 1;
-    //System.out.println("relayneuron " + id + " pairedaxon "+ pairedaxon + " current value "+ currentvalue);    alreadyfired = true;
-}
-return fired;
-}
-public void reset(){
-currentvalue = 0;
-fired = 0;
-alreadyfired = false;
-}
+    public int firecheck() {
+        
+        if (currentvalue > firevalue) {
+            fired = 1;
+            //System.out.println("relayneuron " + id + " pairedaxon "+ pairedaxon + " current value "+ currentvalue);    alreadyfired = true;
+        }
+        return fired;
+    }
+    
+    public void reset() {
+        currentvalue = 0;
+        fired = 0;
+        alreadyfired = false;
+    }
 }
