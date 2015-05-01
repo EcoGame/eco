f70cea0b92323355bbbc0916d23d801c028eb22a
Connor <cbbcbail@gmail.com>
2015-05-01 14:27:22 -0600

    Readability

--- src/eco/neural/OutputNeuron.java
+++ src/eco/neural/OutputNeuron.java
@@ -1,34 +1,42 @@
 package eco.neural;
 
-public class OutputNeuron{
+/**
+ * This class does something
+ *
+ * @author will
+ *
+ */
 
-public int firevalue = 2;
-public int currentvalue;
-public int fired;
-public int id;
-public int pairedaxon;
-public int action = 0;
-public boolean alreadyfired = false;
+public class OutputNeuron {
 
-public void addToCurrent(int x){
+    public int firevalue = 2;
+    public int currentvalue;
+    public int fired;
+    public int id;
+    public int pairedaxon;
+    public int action = 0;
+    public boolean alreadyfired = false;
 
-currentvalue = x + currentvalue;
+    public void addToCurrent(int x) {
+
+        currentvalue = x + currentvalue;
     
-}
-public int firecheck(){
- 
-if(currentvalue > firevalue){
-fired = 1;
-    //System.out.println("outputneuron " + id + " pairedaxon "+ pairedaxon + " current value "+ currentvalue);
-   
-    alreadyfired = true;
-    OutputActions.actions(action);
-}
-return fired;
-}
-public void reset(){
-currentvalue = 0;
-fired = 0;
-    alreadyfired = false;
-}
+    }
+    
+    public int firecheck() {
+        if (currentvalue > firevalue) {
+            fired = 1;
+            //System.out.println("outputneuron " + id + " pairedaxon "+ pairedaxon + " current value "+ currentvalue);
+            alreadyfired = true;
+            OutputActions.actions(action);
+        }
+        return fired;
+    }
+    
+    public void reset() {
+        currentvalue = 0;
+        fired = 0;
+        alreadyfired = false;
+    }
+    
 }
