b87eef182edea8a72f8c8644f81e2f9451b6d355
Nate <4u2njoy.nathan@gmail.com>
2015-05-12 14:35:51 -0600

    Update Loan.java

--- src/eco/game/Loan.java
+++ src/eco/game/Loan.java
@@ -2,8 +2,36 @@ package eco.game;
 
 
 public class Loan {
+    public int maxLoans = 5;
+    public float[] coupons = new float[maxLoans];
+    public int[] principals = new int[maxLoans];
+    public int[] maturityDates = new int[maxLoans];
+
     
     public Loan(){
+        
+        
+    }
+    
+    public void takeLoan(){
+        
+        
+        
+        
+    }
+    
+    public void payInterest(){
+        for(int i = 0; i < maxLoans; i++){
+            if(coupons[i] != 0){
+                
+            }
+        }
+        
+    }
+    
+    public float calculateInterest(){
+        return 4.4f;
+        
         
     }
     
