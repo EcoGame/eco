2af9ae6a770e5cbb1cdc8f69bf249e13102b8d81
Connor <cbbcbail@gmail.com>
2015-05-14 14:37:36 -0600

    Update InputNeuron.java

--- src/eco/neural/InputNeuron.java
+++ src/eco/neural/InputNeuron.java
@@ -1,34 +1,45 @@
 package eco.neural;
 
-public class InputNeuron{
-public int input;
-public int firevalue;
-public int fired;
-public int id;
-public int pairedaxon;
-public int currentvalue;
+/**
+ * A class that handles input
+ *
+ * @author will
+ *
+ */
+
+public class InputNeuron {
+    
+    public int input;
+    public int firevalue;
+    public int fired;
+    public int id;
+    public int pairedaxon;
+    public int currentvalue;
     public boolean alreadyfired = false;
-public boolean highorlow = true; // true = high false = low
+    public boolean highorlow = true; // true = high false = low
 
-public void checkinput(){
+    public void checkinput(){
 
-fired = 0;
-    if(highorlow == true){
-        if(input > firevalue){
-        fired = 1;
-
-        }
-    } else{
-        if(input < firevalue){
         fired = 0;
+        if(highorlow == true) {
+            if(input > firevalue) {
+                fired = 1;
+            }
         }
-
+        else {
+            if(input < firevalue) {
+                fired = 0;
+            }
+        }
+    
+        alreadyfired = true;
     }
-    alreadyfired = true;
-}
-    public void reset(){
+    
+    public void reset() {
+        
         currentvalue = 0;
         fired = 0;
         alreadyfired = false;
+        
     }
 }
