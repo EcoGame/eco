336b3fda22727437d0f0b94431c6f8693f067013
Connor <cbbcbail@gmail.com>
2015-05-14 14:38:15 -0600

    Update KingManager.java

--- src/eco/neural/KingManager.java
+++ src/eco/neural/KingManager.java
@@ -1,85 +1,79 @@
 package eco.neural;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-public class KingManager{
+
+/**
+ * A class that manages the network
+ *
+ * @author will
+ *
+ */
+
+public class KingManager {
+    
     public static int currentnetwork =0 ;
 
-    public static void topmanager(){
+    public static void topmanager() {
 
+        
+        
     }
 
-    public static void gamemanager(){
+    public static void gamemanager() {
+        
         statusreader();
         boolean gamecomplete = false;
         boolean iscomplete0 = false;
         boolean iscomplete1 = false;
-        for(int x = 0; (x< Main.workingnetworks.length) && (iscomplete1 == false); x++){
+        
+        for(int x = 0; (x< Main.workingnetworks.length) && (iscomplete1 == false); x++) {
             if(Main.workingnetworks[x] == true){
                 periodicinit(x);
             }
 
         }
+        
         iscomplete1 = false;
 
-        for(int x = 0; (x< Main.workingnetworks.length) && (iscomplete0 == false); x++){
-            if(Main.workingnetworks[x] == true){
+        for(int x = 0; (x< Main.workingnetworks.length) && (iscomplete0 == false); x++) {
+            if(Main.workingnetworks[x] == true) {
                 currentnetwork = x;
                 Main.workingnetworks[x] = false;
                 iscomplete0 = true;
             }
         }
+        
         iscomplete0 = false;
-        while(gamecomplete == false){
-
-
-
-
-
-
-
+        while(gamecomplete == false) {
 
         }
-
-
-
-
-
     }
 
-
-
-
-
-
-
-
-
-
-
-
-    public static void statusreader(){
+    public static void statusreader() {
+        
         char code;
         int x;
         int generation;
         int success;
 		int filler;
+        
 		try {
 			File file = new File("./eco/txt/currentstates.txt");
 			FileReader fileReader = new FileReader(file);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			StringBuffer stringBuffer = new StringBuffer();
 			String line;
+            
 			while ((line = bufferedReader.readLine()) != null) {
 				code = line.charAt(0);
-
 				String[] tokenSpace = line.split(" ");
-
+                
                 switch(code){
-
                     case 'g':
-                    generation= Integer.parseInt(tokenSpace[1]);
+                        generation= Integer.parseInt(tokenSpace[1]);
                         break;
                     case 'y':
                         x = Integer.parseInt(tokenSpace[1]);
@@ -93,39 +87,27 @@ public class KingManager{
                     case 'q':
                         break;
                 }
+                
 			}
+            
 			fileReader.close();
 
-		} catch (IOException e) {
+		}
+        
+        catch (IOException e) {
 			e.printStackTrace();
 		}
-
-
-
-
     }
 
+    public static void periodicinit(int currentnetwork){
 
-
-public static void periodicinit(int currentnetwork){
-
-NeuronReader.connectionreader(currentnetwork);
-
-NeuronReader.attributereader(currentnetwork);
-
-
-
-NeuralManager.axonpairing(currentnetwork);
-NeuralManager.neuromaker(currentnetwork);
-
-for(int k=0; Main.axonArray.length > k; k++){
-
-ConnectionHandler.connectionmaker(k, Main.axonArray[currentnetwork][k].typebondedto, currentnetwork );
-
-}
-
-
-
-
-}
+        NeuronReader.connectionreader(currentnetwork);
+        NeuronReader.attributereader(currentnetwork);
+        NeuralManager.axonpairing(currentnetwork);
+        NeuralManager.neuromaker(currentnetwork);
+        
+        for(int k=0; Main.axonArray.length > k; k++){
+            ConnectionHandler.connectionmaker(k, Main.axonArray[currentnetwork][k].typebondedto, currentnetwork );
+        }
+    }
 }
