92493370debce1008b2247c3b1f173d76f156f20
Connor <cbbcbail@gmail.com>
2015-05-14 14:35:56 -0600

    Update Axon.java

--- src/eco/neural/Axon.java
+++ src/eco/neural/Axon.java
@@ -1,88 +1,81 @@
 package eco.neural;
 
+/**
+ * A class that handles Axons
+ *
+ * @author will
+ *
+ */
+
 public class Axon{
+    
     int currentnetwork;
-int id;
-int pairedneuronid;
-int[][] connections = new int[5][2];
-int typebondedto=0;
+    int id;
+    int pairedneuronid;
+    int[][] connections = new int[5][2];
+    int typebondedto=0;
     int fired;
     int currentvalue =0;
     int transfervalue=0;
-boolean isused = false;
+    
+    boolean isused = false;
 
     public void pullfires(int currentnetwork){
-                switch(typebondedto){
-
+        
+        switch(typebondedto){
             case 0:
                 int q = 0;
-
                 for(int r = 0; r < Main.axonArraytofill[typebondedto][pairedneuronid- Main.INPUTIDOFFSET].length; r++){
                     q= connections[r][0];
-
                     transfervalue = transfervalue + (connections[r][1] * Main.inputNeuralArray[currentnetwork][q].fired);
-
                 }
-
                 break;
 
             case 1:
                 int e = 0;
-
                 for(int r = 0; r < Main.axonArraytofill[typebondedto][pairedneuronid- Main.OUTPUTIDOFFSET].length; r++){
-
-
                     e=connections[r][0];
-
                     transfervalue = transfervalue + (connections[r][1] * Main.axonArray[currentnetwork][e].fired);
-
                 }
                 break;
 
             case 2:
                 int k = 0;
-
                 for(int r = 0; r < Main.axonArraytofill[typebondedto][pairedneuronid- Main.RELAYIDOFFSET].length; r++){
-
                     k=connections[r][0];
-
                     transfervalue = transfervalue + (connections[r][1] * Main.axonArray[currentnetwork][k].fired);
-
                 }
                 break;
-
         }
 
     }
 
     public void neuroncheck(int currentnetwork){
+        
         int k= 0;
-            switch(typebondedto){
+        switch(typebondedto){
+            case 0:
+                k = pairedneuronid - Main.INPUTIDOFFSET;
+                fired = Main.inputNeuralArray[currentnetwork][k].fired;
+                break;
 
-                case 0:
-                    k = pairedneuronid - Main.INPUTIDOFFSET;
-                    fired = Main.inputNeuralArray[currentnetwork][k].fired;
-
-                    break;
-
-                case 1:
-                    k = pairedneuronid - Main.OUTPUTIDOFFSET;
-                    fired = Main.outputNeuralArray[currentnetwork][k].fired;
-
-                    break;
-
-                case 2:
-                     k = pairedneuronid - Main.RELAYIDOFFSET;
-                    fired = Main.neuralArray[currentnetwork][k].fired;
-
-                    break;
-
-            }
+            case 1:
+                k = pairedneuronid - Main.OUTPUTIDOFFSET;
+                fired = Main.outputNeuralArray[currentnetwork][k].fired;
+                break;
+                
+            case 2:
+                k = pairedneuronid - Main.RELAYIDOFFSET;
+                fired = Main.neuralArray[currentnetwork][k].fired;
+                break;
+        }
 
     }
+    
     public void reset(){
+        
         transfervalue = 0;
-    currentvalue = 0;
-    fired = 0;
+        currentvalue = 0;
+        fired = 0;
     }
 }
