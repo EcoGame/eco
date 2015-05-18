80e7e8fc3a932ac7f8e7f826d0122d3657670309
Retro-Frost <supacow101@gmail.com>
2015-05-18 14:09:46 -0600

    Update GeneticMaster.java

--- src/eco/neural/GeneticMaster.java
+++ src/eco/neural/GeneticMaster.java
@@ -1,25 +1,23 @@
 package eco.neural;
 public class GeneticMaster{
--
--    public static void genemaster(){
--
--
--    }
--
--
--    public static void attributebreeder(int network1, int network2){
--
--
--    }
--
--    public static void connectionbreeder(int network1, int network2){
--
--
--    }
--
--    public static void selectparents(){
--        for(int x =0; x < Main.workingnetworks.length;  x++){
--            //pull score for specific network}}
+
+    public static void genemaster(){
+
+
+    }
+
+
+    public static void attributebreeder(int network1, int network2){
+
+    }
+    public static void connectionbreeder(int network1, int network2){
+
+
+    }
+
+    public static void selectparents(){
+        for(int x =0; x < Main.workingnetworks.length;  x++){
+            //pull score for specific network}}
     }
 }
 }
