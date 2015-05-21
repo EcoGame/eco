3417885b85443c934f32ca61e29b01beff2a0def
Nate <4u2njoy.nathan@gmail.com>
2015-05-21 14:23:34 -0600

    Update SplashText.java

--- src/eco/game/SplashText.java
+++ src/eco/game/SplashText.java
@@ -10,7 +10,7 @@ public class SplashText {
 	
 	static{
 		splashes = new String[]{
-				"95% Stable",
+				"95% Stabe",
 				"Thousands of bugs!",
 				"Artifical but not intelligent",
 				"java.lang.nullPointerException: splash not found. At: SplashText.java(15)",
