127aaae44c84665de2565140cbc87ffd8f821845
Phillip <phillipheikoop@gmail.com>
2015-05-25 19:07:39 -0600

    Update War.java

--- src/eco/game/War.java
+++ src/eco/game/War.java
@@ -122,5 +122,98 @@ public class War {
 			moneyLoss = -money;
 		}
 	}
+	
+	public static void warWith(Country a, Country b){
+		// Book-keeping stuff
+		Log.log(PlayerCountry.year, a.name + " declares war on "
+				+ b.name + "!");
+
+		int diff = a.aggression.aggressionScore - b.aggression.aggressionScore;
+		diff = (int) Math.ceil(diff / (float) aggressionConstant);
+		a.aggression.aggressionScore += diff;
+
+		// Get army sizes
+		int playerArmy = a.warrior.getwPop();
+		int otherArmy = b.warrior.getwPop();
+
+		// Apply some rng
+		playerArmy *= (Math.max(World.random.nextFloat(), 0.5f));
+		otherArmy *= (Math.max(World.random.nextFloat(), 0.5f));
+
+		// Calculate outcome
+		// result > 0: player win
+		// result < 0: country win
+		// result = 0: stalemate
+		int result = playerArmy - otherArmy;
+
+		// Calculate and apply losses
+		if (result > 0) {
+			playerArmy *= Math.min(World.random.nextFloat() * 4, 0.5f);
+			otherArmy *= World.random.nextFloat();
+		} else if (result < 0) {
+			otherArmy *= Math.min(World.random.nextFloat() * 4, 0.5f);
+			playerArmy *= World.random.nextFloat();
+		} else {
+			otherArmy *= World.random.nextFloat();
+			playerArmy *= World.random.nextFloat();
+		}
+		a.warrior.setwPop(playerArmy);
+		a.warrior.setFloatWPop(playerArmy);
+		b.warrior.setwPop(otherArmy);
+		b.warrior.setFloatWPop(otherArmy);
+
+		// Reparations
+		if (result == 0) {
+			Log.log(PlayerCountry.year, "The war with " + b.name
+					+ " ends in stalemate!");
+			winLose = 0;
+			return;
+		} else if (result > 0) {
+			winLose = 1;
+			int wheat = Math.min(b.wheat.gettWheat(), result * 4);
+			int money = Math.min(b.economy.getTreasury(), result * 16);
+			int land = Math.min(b.landsize + b.land.getLand(), result * 2);
+			a.wheat.settWheat(a.wheat.gettWheat()
+					+ wheat);
+			a.economy.setTreasury(a.economy
+					.getTreasury() + money);
+			a.land.addLand(land);
+			b.wheat.settWheat(b.wheat.gettWheat() - wheat);
+			b.economy.setTreasury(b.economy.getTreasury() - money);
+			b.takeLand(land);
+			Log.log(PlayerCountry.year,
+					a.name + " " + NameGen.generateWarWin() + " "
+							+ b.name + "!");
+			wheatLoss = wheat;
+			landLoss = land;
+			moneyLoss = money;
+		} else {
+			winLose = -1;
+			int wheat = Math.min(a.wheat.gettWheat(), -result * 4);
+			int money = Math.min(a.economy.getTreasury(),
+					-result * 16);
+			int land = Math.min(a.landsize + a.land.getLand(), -result * 2);
+			a.wheat.settWheat(a.wheat.gettWheat()
+					- wheat);
+			a.economy.setTreasury(a.economy
+					.getTreasury() - money);
+			a.land.addLand(-land);
+			b.takeLand(-land);
+			b.wheat.settWheat(b.wheat.gettWheat() + wheat);
+			b.economy.setTreasury(b.economy.getTreasury() + money);
+			Log.log(PlayerCountry.year,
+					a.name + " " + NameGen.generateWarLoss() + " "
+							+ b.name + "!");
+			if (a.land.getLand() < minLand) {
+				a.dead = true;
+			}
+			wheatLoss = -wheat;
+			landLoss = -land;
+			moneyLoss = -money;
+		}
+		if (b.land.getLand() < minLand) {
+			b.dead = true;
+		}
+	}
 
 }
