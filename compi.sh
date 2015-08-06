3b77c84741df7ed5124a99e5c0ddf41ded89ea60
Phillip Heikoop <phillipheikoop@gmail.com>
2015-08-06 15:28:36 -0600

    Updated compi, it now compiles into a runnable jar. If you run ./compi clean it will clean up non-code files too.

--- compi.sh
+++ compi.sh
@@ -4,10 +4,22 @@ cd $(dirname "$0")
 if ! [ -d "bin" ]; then
   mkdir bin
 fi
-echo Compiling...
-javac -d bin -cp .:lib/* src/eco/**/*.java
-echo Compiled...
-cd bin
-echo Running...
-java -cp .:../lib/* -Djava.library.path=../native eco/game/Main
-cd eco
+if ! [ -d "jars" ]; then
+  mkdir jars
+fi
+echo "Compiling..."
+javac -Xlint:unchecked -d bin -cp .:lib/* src/eco/**/*.java
+echo "Compiled"
+echo "Creating jar..."
+jar cfm jars/eco.jar src/Manifest.txt -C bin .
+echo "Running..."
+cd jars
+java -Djava.library.path=../native -jar eco.jar
+if [ "$1" = "clean" ]; then
+  cd ..
+  rm -rf bin
+  rm -rf saves
+  rm -rf savetxt
+  rm -rf screenshots
+  echo "Cleaned"
+fi
